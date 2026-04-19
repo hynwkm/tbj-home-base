5e3e7ef253ff62784b5738a42eebae008013a928
Hyun Woo Kim <hynwkm@gmail.com>
2026-04-19 12:08:57 -0400

    add missing fields to pitcher stats

--- app/services/players/pitching.py
+++ app/services/players/pitching.py
@@ -6,11 +6,18 @@ def parse_pitching_stats(stat):
 
     return {
         "g_played": stat.get("gamesPlayed"),
+        "g_started": stat.get("gamesStarted"),
         "ip": stat.get("inningsPitched"),
         "bf": stat.get("battersFaced"),
         "bb": stat.get("baseOnBalls"),
         "strikeouts": stat.get("strikeOuts"),
         "era": stat.get("era") or stat.get("earnedRunAverage"),
+        "whip": stat.get("whip"),
         "so_pct": calculate_rate(stat.get("strikeOuts", 0), bf),
         "bb_pct": calculate_rate(stat.get("baseOnBalls", 0), bf),
+        "so_9": stat.get("strikeoutsPer9Inn"),
+        "bb_9": stat.get("walksPer9Inn"),
+        "so_bb": stat.get("strikeoutWalkRatio"),
+        "hr_allowed": stat.get("homeRuns"),
+        "hr_9": stat.get("homeRunsPer9"),
     }
